e41d17a0520cdd16cdafa3eef46e8b913d1cd59f
root <root@localhost>
2022-09-16 05:13:33 +0000

    Fixing Heroku

--- test_app/app/models/book.rb
+++ test_app/app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ApplicationRecord
+    #Validation for all attributes
     validates :title, presence: true
+    validates :author, presence: true
+    validates :price, presence: true
+    validates :published, presence: true
 end
--- test_app/spec/feature/integration_spec.rb
+++ test_app/spec/feature/integration_spec.rb
@@ -4,9 +4,51 @@ require 'rails_helper'
 RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
-    fill_in 'Title', with: 'harry potter'
+    fill_in 'Title', with: 'Harry potter'
+    fill_in 'Author', with: 'JK Rowling'
+    fill_in 'Price', with: 12
+    fill_in 'Published', with: '9/01/22'
     click_on 'Create Book'
     visit books_path
-    expect(page).to have_content('harry potter')
+    expect(page).to have_content('Harry potter')
+  end
+end
+
+RSpec.describe 'Creating a book', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'Harry potter 2'
+    fill_in 'Author', with: 'JK Rowling'
+    fill_in 'Price', with: 12
+    fill_in 'Published', with: '9/01/22'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('JK Rowling')
+  end
+end
+
+RSpec.describe 'Creating a book', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'Harry potter 3'
+    fill_in 'Author', with: 'JK Rowling'
+    fill_in 'Price', with: 12
+    fill_in 'Published', with: '9/01/22'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('12')
+  end
+end
+
+RSpec.describe 'Creating a book', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'Harry potter 4'
+    fill_in 'Author', with: 'JK Rowling'
+    fill_in 'Price', with: 12
+    fill_in 'Published', with: '9/01/22'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('0009-01-22')
   end
 end
